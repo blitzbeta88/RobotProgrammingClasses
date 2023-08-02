06110c67af454e1d26b0fcad4a89c31feb97aa65
blitzbeta88 <b.gittingerlinzer@gmail.com>
2023-08-01 21:16:09 -0400

    Upload

--- DemoAuto.java
+++ DemoAuto.java
@@ -41,7 +41,6 @@ public class DemoAuto extends LinearOpMode {
 
             position = detector.position;
             telemetry.addData("Positon", position);
-            telemetry.addData("totalA", detector.totalA);
             telemetry.update();
 
             dashboardTelemetry.addData("position", position);
@@ -52,6 +51,15 @@ public class DemoAuto extends LinearOpMode {
 
 
         waitForStart();
+
+        if(position.equals("Location 1")) {
+          //Goes to position 1
+        } else if (position.equals("Location 2")) {
+          //Goes to position 2
+        } else if (position.equals("Location 3")) {
+            //Goes to position 3
+        }
+
 
         move(62,0.5);
 
